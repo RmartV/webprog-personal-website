9375d2adc81958f8e0c1322d92b991424ffc7180
RmartV <134070727+RmartV@users.noreply.github.com>
2024-12-18 10:01:29 +0000

    update myscript

--- home/js/myScript.js
+++ home/js/myScript.js
@@ -1,3 +1,10 @@
 function myFunction() {
-    document.getElementById("demo").innerHTML = "Paragraph changed.";
-  }
+  document.getElementById("demo").innerHTML = "Ray Mart";
+}
+
+function myFunction2() {
+  const element = document.getElementsByTagName("c");
+
+  document.getElementById("demo").innerHTML = 'Its ' + element[0].innerHTML;
+
+}
